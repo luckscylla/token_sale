99aa2812f692f16b01407906f97bdf8fc185ef6f
luckscylla <luckscylla@gmail.com>
2019-03-15 16:47:33 +0800

    1. Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract DappToken {
+	// contstructor
+	uint public totalSupply;
+	constructor() public {
+		totalSupply = 1000000;
+
+	}
+
+	// set the total number of tokens
+	// get the total number of tokens
+}
