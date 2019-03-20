565a2e856a5e1514cd0c264b6100bc50024ced6a
luckscylla <luckscylla@gmail.com>
2019-03-20 17:24:12 +0800

    update front-end page

--- src/js/app.js
+++ src/js/app.js
@@ -1,93 +1,124 @@
+
 App = {
-	web3Provider: null,
 	contracts: {},
 	account: '0x0',
 	tokenPrice: 1000000000000000,
 	tokensSold: 0,
 	tokensAvailable: 750000,
+	tokenContractAddr: "0x2F5FdC715A2dbC91C2916f84c6B92c38Eb5934b0",
+	tokenSaleContractAddr: "0x1EB43811725A004BcC0e506f228bc6a655a4658e",
 
 	init: function() {
-		console.log("App initialized ...");
+		console.log("App initializing ...");
 		return App.initWeb3();
 	},
 
 	initWeb3: function() {
-		// If a web3 instance is already provided by Meta Mask.
 		if (typeof web3 !== 'undefined') {
-			console.log("using meta mask: " + web3);
-			console.log(window.web3.currentProvider)
-			App.web3Provider = window.web3.currentProvider;
-			web3 = new Web3(window.web3.currentProvider);
-
-		// Specify default instance if no web3 instance provided
+			web3.currentProvider.on('accountsChanged', function (accounts) {
+				console.log("account changed: " + accounts[0]);
+				App.account = accounts[0];
+				return App.render();
+			});
+			if(web3.eth.accounts[0] == null) {
+				console.log("user account is not found !");
+			} else {
+				console.log("user account: " + web3.eth.accounts[0]);
+				App.account = web3.eth.accounts[0];
+				return App.initContract();
+			}
 		} else {
-			App.web3Provider = new Web3.providers.WebsocketProvider('ws://localhost:7545');
-			web3 = new Web3(App.web3Provider);
+			console.log("web3 service not found !");
 		}
-		return App.initContract();
 	},
 
 	initContract: function() {
-		$.getJSON("DappTokenSale.json", function(dappTokenSale) {
-			App.contracts.DappTokenSale = TruffleContract(dappTokenSale);
-			App.contracts.DappTokenSale.setProvider(App.web3Provider);
-			App.contracts.DappTokenSale.deployed().then(function(dappTokenSale) {
-				console.log("Dapp Token Sale Address:", dappTokenSale.address);
-			});
-		}).done(function() {
-			$.getJSON("DappToken.json", function(dappToken) {
-				App.contracts.DappToken = TruffleContract(dappToken);
-				App.contracts.DappToken.setProvider(App.web3Provider);
-				App.contracts.DappToken.deployed().then(function(dappToken) {
-					console.log("Dapp Token Address:", dappToken.address);
-				});
+		$.getJSON("DappToken.abi", function(abi) {
+			App.contracts.DappToken = web3.eth.contract(abi).at(App.tokenContractAddr);
+			$.getJSON("DappTokenSale.abi", function(abi) {
+				App.contracts.DappTokenSale = web3.eth.contract(abi).at(App.tokenSaleContractAddr);
+				App.listenForEvents();
 				return App.render();
 			});
 		});
 	},
 
-	render: function() {
-		$('#loader').show();
+	listenForEvents: function() {
+		let sellEvent = App.contracts.DappTokenSale.Sell();
+		sellEvent.watch(function(error, result) {
+			if(!error) {
+				$('#status').html("Token transaction is done !");
+				return App.render();
+			} else {
+				console.log(error);
+			}
+		});
+	},
 
-		// load account data
-		web3.eth.getCoinbase(function(err, account) {
-			if(err === null) {
-				App.account = account;
-				$('#accountAddress').html("Your Account: " + account);
+	render: function() {
+		$('#accountAddress').html(App.account);
+
+		web3.eth.getBalance(App.account, function(error, result) {
+			if(!error) {
+				// console.log("balabce: " + result);
+				balance = result.toNumber();
+				$('.eth-balance').html(web3.fromWei(balance, "ether"));
+			} else {
+				console.log(error);
 			}
 		});
 
-		App.contracts.DappTokenSale.deployed().then(async function(instance) {
-			tokenPrice = await instance.tokenPrice();
-			App.tokenPrice = tokenPrice.toNumber();
-			$('.token-price').html(web3.fromWei(App.tokenPrice, "ether"));
-			tokensSold = await instance.tokensSold();
-			// App.tokensSold = 500000;
-			App.tokensSold = tokensSold.toNumber();
-			$('.tokens-sold').html(App.tokensSold);
-			$('.tokens-available').html(App.tokensAvailable);
-			let progressPercent = (App.tokensSold / App.tokensAvailable) * 100;
-			$('#progress').css('width', progressPercent + '%');
+		App.contracts.DappToken.balanceOf(App.account, function(error, result) {
+			if(!error) {
+				// console.log("balabce: " + result);
+				balance = result.toNumber();
+				$('.dapp-balance').html(balance);
+			} else {
+				console.log(error);
+			}
 		});
 
-		App.contracts.DappToken.deployed().then(async function(instance) {
-			balance = await instance.balanceOf(App.account);
-			$('.dapp-balance').html(balance.toNumber());
-			$('#loader').hide();
+		App.contracts.DappTokenSale.tokenPrice(function(error, result) {
+			if(!error) {
+				// console.log("token-price: " + result);
+				App.tokenPrice = result.toNumber();
+				$('.token-price').html(web3.fromWei(App.tokenPrice, "ether"));
+			} else {
+				console.log(error);
+			}
+		});
+
+		App.contracts.DappTokenSale.tokensSold(function(error, result) {
+			if(!error) {
+				// console.log("tokens-sold: " + result);
+				App.tokensSold = result.toNumber();
+				$('.tokens-sold').html(App.tokensSold);
+				$('.tokens-available').html(App.tokensAvailable);
+				let progressPercent = (App.tokensSold / App.tokensAvailable) * 100;
+				$('#progress').css('width', progressPercent + '%');
+			} else {
+				console.log(error);
+			}
 		});
 
 	},
 
 	buyTokens: function() {
-		var numberOfTokens = $('#numberOfTokens').val();
-		App.contracts.DappTokenSale.deployed().then(async function(instance) {
-			result = await instance.buyTokens(numberOfTokens, {
+		let numberOfTokens = $('#numberOfTokens').val();
+		let sender = {
 				from: App.account, 
 				value: numberOfTokens * App.tokenPrice,
 				gas: 500000	// gas limit
-			});
-			console.log("Tokens bought ... ");
-			return App.render();
+			};
+		$('#status_bar').show();
+		$('#status').html("Token transaction is waiting confirmation ...");
+		console.log("Buy Tokens ... ");
+		App.contracts.DappTokenSale.buyTokens(numberOfTokens, sender, function(error, result) {
+			if(!error) {
+				$('#status').html("Token transaction is under processing ...");
+			} else {
+				console.log(error);
+			}
 		});
 	},
 
